05ed3cf3d256ad0c604aa3fc055dab5e851287c2
Maxime Mouchet <max@maxmouchet.com>
2022-05-21 09:23:20 +0200

    update

--- scripts/iris-exporter-raw.sh
+++ scripts/iris-exporter-raw.sh
@@ -3,29 +3,20 @@
 # shellcheck source=scripts/common.sh
 . "${BASH_SOURCE%/*}/common.sh"
 
-count_query() {
-    cat <<EOF
-    SELECT COUNT() FROM results__$1
-EOF
-}
-
-results_query() {
-    cat <<EOF
-    SELECT * FROM results__$1 FORMAT CSV
-EOF
-}
-
 if [ $# -ne 2 ]; then
     echo "$0 MEASUREMENT_UUID AGENT_UUID"
     exit 1
 fi
 
-measurement_id="${1//-/_}__${2//-/_}"
-query=$(traceroutes_query "$measurement_id")
-total=$(clickhouse "$(count_query "$measurement_id")")
+test_aws
+test_clickhouse
 
-#url="s3://${AWS_S3_BUCKET}/$1__$2__atlas.jsonl.zst"
-#clickhouse "${query}" | pv "${total}" "iris-to-atlas" | iris-to-atlas | zstd | aws s3 cp - "${url}" || aws s3 rm "${url}"
-#
-#url="s3://${AWS_S3_BUCKET}/$1__$2__warts-trace.warts.zst"
-#clickhouse "${query}" | pv "${total}" "iris-to-warts-trace" | iris-to-warts-trace | zstd | aws s3 cp - "${url}" || aws s3 rm "${url}"
+measurement_id="${1//-/_}__${2//-/_}"
+url="s3://${AWS_S3_BUCKET}/$1__$2__csv.csv.zst"
+
+query="SELECT * FROM results__${measurement_id} FORMAT CSVWithNames"
+total=$(clickhouse "SELECT COUNT() FROM results__${measurement_id}")
+
+if $(aws_does_not_exists "${url}"); then
+  clickhouse "${query}" | pv "${total}" | zstd | aws s3 cp - "${url}" || aws s3 rm "${url}"
+fi
